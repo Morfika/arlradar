cfe21a0f51cd5ad9477953bdf8e8ed980175ed6c
Brayan Camilo Joya Herrea <b.joya@uniandes.edu.co>
2026-05-07 12:48:06 -0500

    Vercel Deploy

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -79,7 +79,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
       { property: "og:description", content: "De 87.000 conversaciones a decisiones." },
       { property: "og:type", content: "website" },
       { name: "twitter:card", content: "summary" },
-      { name: "twitter:site", content: "@Lovable" },
+      { name: "twitter:site", content: "@ARL_Radar" },
     ],
     links: [
       {
